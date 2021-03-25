ea506c6154473f82362fdbe307301512ce99e15b
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-25 10:38:08 +0700

    Update ChatFeed.jsx

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -45,20 +45,21 @@ const ChatFeed = (props) => {
 
   if (!chat) return "Loading";
 
-  return;
-  <div className="chat-feed">
-    <div className="chat-title-container">
-      <div className="chat-title">{chat?.title}</div>
-      <div className="chat-subtitle">
-        {chat.people.map((person) => ` ${person.person.username}`)}
+  return (
+    <div className="chat-feed">
+      <div className="chat-title-container">
+        <div className="chat-title">{chat?.title}</div>
+        <div className="chat-subtitle">
+          {chat.people.map((person) => ` ${person.person.username}`)}
+        </div>
+      </div>
+      {renderMessages()}
+      <div style={{ height: "100px" }} />
+      <div className="message-form-container">
+        <MessageForm {...props} chatId={activeChat} />
       </div>
     </div>
-    {renderMessages()}
-    <div style={{ height: "100px" }} />
-    <div className="message-form-container">
-      <MessageForm {...props} chatId={activeChat} />
-    </div>
-  </div>;
+  );
 };
 
 export default ChatFeed;
