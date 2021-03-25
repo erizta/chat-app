41692b80a0687bd4cdd2bf0f2837fb1890e7f8ba
Erizta Alifa P <33772785+erizta@users.noreply.github.com>
2021-03-25 11:16:17 +0700

    Update MessageForm.jsx

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -22,6 +22,10 @@ const MessageForm = (props) => {
     isTyping(props, chatId);
   };
 
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: "" });
+  };
+
   return (
     <form className="message-form" onSubmit={handleSubmit}>
       <input
@@ -34,9 +38,19 @@ const MessageForm = (props) => {
       />
       <label htmlFor="upload-button">
         <span className="image-button">
-            <PictureOutlined className="picture-icon" />
+          <PictureOutlined className="picture-icon" />
         </span>
       </label>
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload}
+      />
+      <button type="submit" className="Send-button">
+        <SendOutlined className="send-icon" />
+      </button>
     </form>
   );
 };
